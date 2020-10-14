b3ba957971b106cd09c3675857b8c49069f9dc1c
Alan Lam <s3436174@student.rmit.edu.au>
2020-10-15 01:33:46 +1100

    Complete and cleanup code

--- main.cpp
+++ main.cpp
@@ -19,34 +19,112 @@ struct Name {
 
 };
 
-void createSbrkBlock(list<Name*> &memList, int wsize, const char* token);
 
+/**
+ * Uses First Fit Strategy to allocate new elements into memory
+ * @param allocMBList reference allocMBList to store used memory pointers
+ * @param freedMBList reference freedMBList to store free memory pointers
+ * @param wsize int size of the element to be stored
+ * @param token pointer to the element to be stored
+ * @return true if successfully allocated
+*/
 bool firstFitStrategy(list<Name*> &allocMBList, list<Name*> &freedMBList, int wsize, const char* token);
 
+/**
+ * Uses Best Fit Strategy to allocate new elements into memory
+ * After sorting the freedMBList from smallest to largest memory size it calls on 
+ * firstFitStrategy method to allocate as it uses the same logic.
+ * @param allocMBList reference allocMBList to store used memory pointers
+ * @param freedMBList reference freedMBList to store free memory pointers
+ * @param wsize int size of the element to be stored
+ * @param token pointer to the element to be stored
+ * @return true if successfully allocated
+*/
 bool bestFitStrategy(list<Name*> &allocMBList, list<Name*> &freedMBList, int wsize, const char* token);
 
+/**
+ * Uses Worst Fit Strategy to allocate new elements into memory.
+ * After sorting the freedMBList from largest to smallest memory size it calls on 
+ * firstFitStrategy method to allocate as it uses the same logic.
+ * @param allocMBList reference allocMBList to store used memory pointers
+ * @param freedMBList reference freedMBList to store free memory pointers
+ * @param wsize int size of the element to be stored
+ * @param token pointer to the element to be stored
+ * @return true if successfully allocated
+*/
 bool worstFitStrategy(list<Name*> &allocMBList, list<Name*> &freedMBList, int wsize, const char* token);
 
+/**
+ * Allocates new block of memory using sbrk() and adds it to list
+ * @param memList reference list to add new element
+ * @param wsize int size of memory block to be created
+ * @param token pointer to element to be stored
+*/
+void createSbrkBlock(list<Name*> &memList, int wsize, const char* token);
+
+/**
+ * Used to combine any memory that are adjacent to each other in the list
+ * @param freedMBList list of elements to combine
+ * @return false if there are no more elements to combine
+*/
 bool combineMemoryBlock(list<Name*> &freedMBList);
 
+/**
+ * Splits element into smaller memory blocks to allocate and saves them to the lists after allocation
+ * @param allocMBList reference list to save new element/memory block
+ * @param freedMBList reference list to save split memory block
+ * @param it iterator location of element to be split
+ * @param wsize int size of memory block to be stored
+ * @param token pointer to element to be stored
+*/
 void splitMemoryBlock(list<Name*> &allocMBList, list<Name*> &freedMBList, list<Name*>::iterator it, int wsize, const char* token);
 
-
-// ------ Helper methods ------
 /**
- * Helps sort the memory addresses stored in a list in order
+ * Sorts list based on memory address
  * @param memList reference list to be sorted
 */
 void sortByMemAddress(list<Name*> &memList);
 
+/**
+ * Sorts list based on smallest to largest memory size
+ * @param memList reference list to be sorted
+*/
 void sortByMemSizeSmallToLarge(list<Name*> &memList);
 
+/**
+ * Sorts list based on largest to smallest memory size
+ * @param memList reference list to be sorted
+*/
 void sortByMemSizeLargeToSmall(list<Name*> &memList);
 
+/**
+ * Randomly removes elements from one list to another
+ * @param allocMBList reference allocMBList to remove from
+ * @param freedMBList reference freedMBList to move to
+ * @param count int number of elements to remove
+*/
 void randomRemoval(list<Name*> &allocMBList, list<Name*> &freedMBList, int count);
 
+/**
+ * Outputs data to a CSV file
+ * @param filename string name of file to be output to
+ * @param total int total amount of memory used
+*/
+void outputCSV(list<Name*> allocMBList, list<Name*> freedMBList, string filename, int total);
+
+/**
+ * Finds the total memory used by adding the size in each list
+ * @param allocMBList allocMBList to be added
+ * @param freedMBList freedMBList to be added
+ * @return int total of memory size
+*/
+int findTotalSize(list<Name*> allocMBList, list<Name*> freedMBList);
+
+/**
+ * Help for testing and prints out list
+ * @param memList list to be printed out
+*/
 void printMBList(list<Name*> memList);
-// ----------------------------
 
 
 int main(int argc, char** argv) {
@@ -59,29 +137,33 @@ int main(int argc, char** argv) {
     list<Name*> allocMBList;
     list<Name*> freedMBList;
 
-    if (argc != 3) {
+    if (argc != 4) {
         cout << "Error - requires two arguments to run." << endl;
-        cout << "Run program with the input: ./main <strategy> <filename>" << endl;
+        cout << "Run program with the input: ./main <strategy> <input> <output>" << endl;
         cout << "<scheduler> selection: -first | -best | -worst" << endl;
-        cout << "<filename>: Name of file including extension" << endl;
+        cout << "<input>: Name of input file including extension" << endl;
+        cout << "<output>: Name of output file including extension" << endl;
         return EXIT_FAILURE;
+
     }
+
+    string outputFileName = argv[3];
 
     infile.open(argv[2]);
 
     if (infile.fail()) {
-        cout << "Error - could not open file" << endl;
+        cout << "Error - could not open input file" << endl;
         cout << "Did you forget the extension?" << endl;
         return EXIT_FAILURE;
 
     } else {
 
-        // Start allocating to list
         int counter = 0;
 
-        std::string line;
+        string line;
         const char* token;
 
+        // Start allocating to lists
         while (!infile.eof()) {
 
             std::getline(infile, line);
@@ -89,12 +171,10 @@ int main(int argc, char** argv) {
 
             size_t wsize = strlen(token) + 1;
 
-            if (counter < 100) {
-
+            if (counter < 1000) {
                 createSbrkBlock(allocMBList, wsize, token);
 
-            // } else if (counter >= 100 && ((counter+1) % 100 == 0)) { // For every 500
-            } else if (counter >= 100) {
+            } else if (counter >= 1000) {
 
                 if (argv[1] == string("-first")) {
                     if (!firstFitStrategy(allocMBList, freedMBList, wsize, token)) {
@@ -117,7 +197,7 @@ int main(int argc, char** argv) {
 
             ++counter;
 
-            if (counter == 1000) {
+            if (counter % 1000 == 0) {
                 // Remove randomly
                 randomRemoval(allocMBList, freedMBList, 500);
                 
@@ -130,33 +210,24 @@ int main(int argc, char** argv) {
                     combined = combineMemoryBlock(freedMBList);
                 }
 
-            } else if (counter > 1000 && counter % 1000 == 0) {
-
-                // Remove
-                randomRemoval(allocMBList, freedMBList, 50);
-
-                // Sort
-                sortByMemAddress(freedMBList);
-
-                // Combine
-                bool combined = true;
-                while (combined) {
-                    combined = combineMemoryBlock(freedMBList);
-                }
-
-            }
+            } 
 
         }
 
     }
 
-    cout << "Final alloc list, size: " << allocMBList.size() << endl;
-    printMBList(allocMBList);
-
-    cout << "Final freed list, size: " << freedMBList.size() << endl;
-    printMBList(freedMBList);
-
     infile.close();
+
+    sortByMemAddress(freedMBList);
+    sortByMemAddress(allocMBList);
+
+    int total = findTotalSize(allocMBList, freedMBList);
+
+    outputCSV(allocMBList, freedMBList, outputFileName, total);
+    cout << "Data output to " << outputFileName << endl;
+
+    // Clean up
+    sbrk(-total);
 
     for (Name* name : allocMBList) {
         delete name;
@@ -169,7 +240,7 @@ int main(int argc, char** argv) {
     return EXIT_SUCCESS;
 }
 
-// Find first fit
+
 bool firstFitStrategy(list<Name*> &allocMBList, list<Name*> &freedMBList, int wsize, const char* token) {
 
     list<Name*>::iterator it;
@@ -185,25 +256,24 @@ bool firstFitStrategy(list<Name*> &allocMBList, list<Name*> &freedMBList, int ws
         } else if (wsize < (*it)->size) {
             splitMemoryBlock(allocMBList, freedMBList, it, wsize, token);
             return true;
+
         }
     }
 
     return false;
 }
 
-// Find best fit
 bool bestFitStrategy(list<Name*> &allocMBList, list<Name*> &freedMBList, int wsize, const char* token) {
 
-    sortByMemSizeSmallToLarge(freedMBList); // Order the list by size smallest to largest
+    sortByMemSizeSmallToLarge(freedMBList); // Order the list by size from smallest to largest
 
     return firstFitStrategy(allocMBList, freedMBList, wsize, token);
 
 }
 
-// Find worst fit
 bool worstFitStrategy(list<Name*> &allocMBList, list<Name*> &freedMBList, int wsize, const char* token) {
 
-    sortByMemSizeLargeToSmall(freedMBList);
+    sortByMemSizeLargeToSmall(freedMBList); // Order the list by size from largest to smallest
 
     return firstFitStrategy(allocMBList, freedMBList, wsize, token);
 
@@ -261,23 +331,18 @@ void splitMemoryBlock(list<Name*> &allocMBList, list<Name*> &freedMBList, list<N
 
 }
 
-
-// ------ Helper methods ------
-
 void sortByMemAddress(list<Name*> &memList) {
     memList.sort([](Name* a, Name* b) {
         return (void*)a->wptr < (void*)b->wptr;
     });
 }
 
-// Method to sort by size - smallest to largest
 void sortByMemSizeSmallToLarge(list<Name*> &memList) {
     memList.sort([](Name* a, Name* b) {
         return (uintptr_t)a->size < (uintptr_t)b->size;
     });
 }
 
-// Method to sort by size - largest to smallest
 void sortByMemSizeLargeToSmall(list<Name*> &memList) {
     memList.sort([](Name* a, Name* b) {
         return (uintptr_t)a->size > (uintptr_t)b->size;
@@ -288,9 +353,7 @@ void randomRemoval(list<Name*> &allocMBList, list<Name*> &freedMBList, int count
 
     int randomCount = 0;
 
-    // Randomly move n to freed list
     while (randomCount < count) {
-        // cout << "This random removal works: " << randomCount << endl;
         
         int random = rand() % allocMBList.size();
         list<Name*>::iterator it = std::next(allocMBList.begin(), random);
@@ -302,7 +365,57 @@ void randomRemoval(list<Name*> &allocMBList, list<Name*> &freedMBList, int count
     }
 }
 
-// For printing out the lists
+void outputCSV(list<Name*> allocMBList, list<Name*> freedMBList, string filename, int total) {
+
+    std::ofstream outfile;
+
+    // Output CSV file to filename
+    outfile.open(filename);
+
+    outfile << "Total memory:," << total << endl; 
+    outfile << endl;
+
+    outfile << "allocMBList" << endl;
+    outfile << "Address," << "Size," << "Content" << endl;
+
+    for (Name* name : allocMBList) {
+        outfile << (void*)name->wptr << "," 
+                << name->size << "," 
+                << name->wptr 
+                << endl;
+    }
+
+    outfile << endl;
+
+    outfile << "freedMBList" << endl;
+    outfile << "Address," << "Size" << endl;
+
+    for (Name* name : freedMBList) {
+        outfile << (void*)name->wptr << "," 
+                << name->size
+                << endl;
+    }
+
+    outfile.close();
+
+}
+
+int findTotalSize(list<Name*> allocMBList, list<Name*> freedMBList) {
+
+    int a = 0, b = 0;
+
+    for(Name* name : allocMBList) {
+        a += name->size;
+    }
+
+    for(Name* name : freedMBList) {
+        b += name->size;
+    }
+    
+    return a + b;
+
+}
+
 void printMBList(list<Name*> memList) {
 
     for (Name* name : memList) {
@@ -311,30 +424,3 @@ void printMBList(list<Name*> memList) {
 
     cout << endl;
 }
-
-void outputCSV(list<Name*> mbList, string filename, bool allocMBList) {
-
-    std::ofstream outfile;
-
-    // Output CSV file to filename
-    outfile.open(filename);
-
-    if (allocMBList) {
-        outfile << "Address," << "Size," << "Content" << endl;
-
-        for (Name* name : mbList) {
-            outfile << (void*)name->wptr << "," 
-                    << name->size << "," 
-                    << name->wptr 
-                    << endl;
-        }
-
-    } else {
-
-    }
-
-
-    outfile.close();
-
-}
-
